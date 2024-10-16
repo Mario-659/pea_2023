1dd555760043daa7ab41b4f9aff50704ddbb5915
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-16 19:44:22 +0200

    refactor brute force

--- src/algorithms/BruteForce.cpp
+++ src/algorithms/BruteForce.cpp
@@ -1,11 +1,11 @@
 #include "BruteForce.h"
 
 void BruteForce::findShortestPath(AdjacencyMatrix &graph) {
-    size = graph.getSize();
-    std::vector<int> vertices;
-    shortestPathLength = INT_MAX;
+    shortestPathLength = std::numeric_limits<int>::max();
 
-    // Initialize the list of vertices. Start from 1 since 0 is the starting vertex.
+    size = graph.getSize();
+    // initialize the list of vertices. Start from 1 since 0 is the starting vertex.
+    std::vector<int> vertices;
     for (int i = 1; i < size; i++) {
         vertices.push_back(i);
     }
@@ -13,9 +13,9 @@ void BruteForce::findShortestPath(AdjacencyMatrix &graph) {
     do {
         int currentPathWeight = 0;
         bool routeDoesNotExist = false;
-        int lastVertex = 0;  // Start at vertex 0
+        int lastVertex = 0;  // start at vertex 0
 
-        // Calculate the current path weight
+        // calculate the current path weight
         for (int vertex : vertices) {
             int edgeWeight = graph.getEdge(lastVertex, vertex);
 
@@ -32,14 +32,14 @@ void BruteForce::findShortestPath(AdjacencyMatrix &graph) {
             continue;
         }
 
-        // Add weight of returning to the starting vertex
+        // add weight of returning to the starting vertex
         int returnEdgeWeight = graph.getEdge(lastVertex, 0);
         if (returnEdgeWeight == NO_EDGE) {
             continue;
         }
         currentPathWeight += returnEdgeWeight;
 
-        // Update the shortest path if current path is shorter
+        // update the shortest path if current path is shorter
         if (currentPathWeight < shortestPathLength) {
             shortestPathLength = currentPathWeight;
             bestPath = vertices;
