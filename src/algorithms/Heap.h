8131ecb781524333ec243cb6c4e4539c6edf45e2
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-16 17:15:06 +0200

    update Heap

--- src/algorithms/Heap.h
+++ src/algorithms/Heap.h
@@ -13,11 +13,11 @@ private:
     int maxSize;
     int currSize;
     int getParent(int index) {
-        return (index-1) / 2; }
+        return (index - 1) / 2; }
     int getLeft(int index) {
-        return 2*index + 1; }
+        return 2 * index + 1; }
     int getRight(int index) {
-        return 2*index + 2; }
+        return 2 * index + 2; }
 
     // Restores structure of heap from given index
     void heapify(int index);
@@ -40,12 +40,12 @@ public:
     // Returns current size
     int getSize() { return currSize; }
 
-    ~Heap() { delete arr; }
+    ~Heap() { delete[] arr; }
 };
 
 template <typename T>
 T Heap<T>::extractMin() {
-    if(currSize == 0) return T();
+    if (currSize == 0) return T();
 
     T minValue = arr[0];
     arr[0] = arr[currSize - 1];
@@ -57,19 +57,17 @@ T Heap<T>::extractMin() {
 template <typename T>
 void Heap<T>::push(T value) {
     // Check if maximum size reached
-    if(currSize == maxSize) return;
+    if (currSize == maxSize) return;
 
-    // Add element at the end of an array
     arr[currSize] = value;
+    currSize++;
 
     // Move to higher level if value is smaller than its parent
-    int currInd = currSize;
-    while(arr[getParent(currInd)] > arr[currInd] && currInd != 0){
+    int currInd = currSize - 1;
+    while (currInd != 0 && arr[getParent(currInd)] > arr[currInd]) {
         std::swap(arr[currInd], arr[getParent(currInd)]);
-
         currInd = getParent(currInd);
     }
-    currSize++;
 }
 
 template <typename T>
@@ -81,24 +79,17 @@ Heap<T>::Heap(int maxSize) {
 
 template <typename T>
 void Heap<T>::heapify(int index) {
-    // get children
+    int smallest = index;
     int l = getLeft(index);
     int r = getRight(index);
 
-    // TODO check if it works when
-    if (r > currSize) return;
-
-    int smallest;
-    // check which is the largest: value at index, its left child or its right child
-    if(l <= currSize && arr[l] < arr[index]){
+    if (l < currSize && arr[l] < arr[smallest]) {
         smallest = l;
     }
-    else smallest = index;
-    if(r <= currSize && arr[r] < arr[smallest]){
+    if (r < currSize && arr[r] < arr[smallest]) {
         smallest = r;
     }
-    // if element at index is not the smallest swap with largest and repeat for smallest (down the tree)
-    if(smallest != index){
+    if (smallest != index) {
         std::swap(arr[index], arr[smallest]);
         heapify(smallest);
     }
@@ -106,19 +97,17 @@ void Heap<T>::heapify(int index) {
 
 template<typename T>
 bool Heap<T>::contains(T value) {
-    for(int i=0; i<currSize; i++) {
-        if(arr[i] == value) return true;
+    for (int i = 0; i < currSize; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-// https://stackoverflow.com/questions/46996064/how-to-update-elements-within-a-heap-priority-queue
 template<typename T>
 void Heap<T>::update(T prev, T newVal) {
-
     int indexElement;
-    for(int i=0; i<currSize; i++) {
-        if(arr[i] == prev) {
+    for (int i = 0; i < currSize; i++) {
+        if (arr[i] == prev) {
             arr[i] = newVal;
             indexElement = i;
             break;
@@ -127,13 +116,12 @@ void Heap<T>::update(T prev, T newVal) {
 
     if (newVal > prev) {
         heapify(indexElement);
-    }
-
-    else if (newVal < prev) {
+    } else {
         while (indexElement != 0 && arr[getParent(indexElement)] > newVal) {
             std::swap(arr[indexElement], arr[getParent(indexElement)]);
             indexElement = getParent(indexElement);
         }
     }
 }
-#endif //SDIZO_2_HEAP_H
+
+#endif // SDIZO_2_HEAP_H
