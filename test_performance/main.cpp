dfb0f809674305399be7c8eec835773900ba67d0
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-26 21:06:42 +0200

    add test performance for brute force

--- test_performance/main.cpp
+++ test_performance/main.cpp
@@ -1,30 +1,65 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include "utils/Utils.h"
+#include "algorithms/BruteForce.h"
 
 using namespace std;
+
+long long runBruteForceTest(int graphSize, int density) {
+    long long matrixResult = 0;
+
+    chrono::high_resolution_clock::time_point t1;
+    chrono::high_resolution_clock::time_point t2;
+
+    BruteForce bruteForce;
+
+    int numberOfRuns = 50;
+
+    for (int i=0; i<numberOfRuns; i++) {
+        AdjacencyMatrix* matrixGraph = new AdjacencyMatrix(graphSize);
+
+        utils::generateRandomUndirectedGraphs(matrixGraph, graphSize, density);
+
+        cout << "\rGraph size: " << graphSize << ", density: " << density << ", iteration: " << i;
+
+        t1 = chrono::high_resolution_clock::now();
+
+        bruteForce.findShortestPath(*matrixGraph);
+
+        t2 = chrono::high_resolution_clock::now();
+        auto result = chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
+        matrixResult += result;
+    }
+
+    return matrixResult / numberOfRuns;
+}
+
+void runSample(vector<int> densities, vector<int> sampleSizes) {
+    ofstream output_file;
+    output_file.precision(17);
+    output_file.open("sample.csv");
+
+//    cout                 << "sample" << "," << "density" << "," << "averageTimeInNanoseconds" << "\n";
+    output_file << fixed << "sample" << "," << "density" << "," << "averageTimeInNanoseconds" << "\n";
+
+    for (auto dens : densities)
+        for (auto sample : sampleSizes) {
+            long long result = runBruteForceTest(sample, dens);
+
+//            cout                 << sample << "," << dens << "," << result << "\n";
+            output_file << fixed << sample << "," << dens << "," << result << "\n";
+        }
+    output_file.close();
+}
 
 // prints to csv files size, density, type, time
 int main() {
     vector<int> densities({20, 60, 99});
-    vector<int> sampleSizes({10, 25, 50, 65, 75, 90, 100});
+    vector<int> sampleSizes({4, 5, 6, 7, 8, 9, 10, 11});
 
     cout.precision(30);
-}
 
-void runSample(vector<int> densities, vector<int> sampleSizes) {
-    ofstream file;
-    file.precision(17);
-//    SampleTSP tspTest;
-    file.open("sample.csv");
-    for (auto dens : densities)
-        for (auto sample : sampleSizes) {
-//            tspTest.runKruskalTest(sample, dens);
-//            std::cout << sample << "," << dens << "," << "matrix," << tspTest.getMatrixResult() << "\n";
-//            std::cout << sample << "," << dens << "," << "list," << tspTest.getListResult() << "\n";
-//            file << fixed << sample << "," << dens << "," << "matrix," << tspTest.getMatrixResult() << "\n";
-//            file << fixed << sample << "," << dens << "," << "list," << tspTest.getListResult() << "\n";
-        }
-    file.close();
+    runSample(densities, sampleSizes);
 }
