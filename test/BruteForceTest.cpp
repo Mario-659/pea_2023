a633998885a9a30ac793a21468519700f80fde07
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-11-10 19:25:41 +0100

    add tsp 13 brute force test

--- test/BruteForceTest.cpp
+++ test/BruteForceTest.cpp
@@ -454,3 +454,181 @@ TEST(BruteForceTSPTest, tsp_12_1) {
     ASSERT_EQ(264, tsp.getShortestPathLength());
     ASSERT_EQ("0 -> 1 -> 8 -> 4 -> 6 -> 2 -> 11 -> 9 -> 7 -> 5 -> 3 -> 10 -> 0", tsp.toString());
 }
+
+//TEST(BruteForceTSPTest, tsp_13) {
+//    AdjacencyMatrix graph(13);
+//    graph.setEdge(0, 1, 29);
+//    graph.setEdge(0, 2, 82);
+//    graph.setEdge(0, 3, 46);
+//    graph.setEdge(0, 4, 68);
+//    graph.setEdge(0, 5, 52);
+//    graph.setEdge(0, 6, 72);
+//    graph.setEdge(0, 7, 42);
+//    graph.setEdge(0, 8, 51);
+//    graph.setEdge(0, 9, 55);
+//    graph.setEdge(0, 10, 29);
+//    graph.setEdge(0, 11, 74);
+//    graph.setEdge(0, 12, 23);
+//
+//    graph.setEdge(1, 0, 29);
+//    graph.setEdge(1, 2, 55);
+//    graph.setEdge(1, 3, 46);
+//    graph.setEdge(1, 4, 42);
+//    graph.setEdge(1, 5, 43);
+//    graph.setEdge(1, 6, 43);
+//    graph.setEdge(1, 7, 23);
+//    graph.setEdge(1, 8, 23);
+//    graph.setEdge(1, 9, 31);
+//    graph.setEdge(1, 10, 41);
+//    graph.setEdge(1, 11, 51);
+//    graph.setEdge(1, 12, 11);
+//
+//    graph.setEdge(2, 0, 82);
+//    graph.setEdge(2, 1, 55);
+//    graph.setEdge(2, 3, 68);
+//    graph.setEdge(2, 4, 46);
+//    graph.setEdge(2, 5, 55);
+//    graph.setEdge(2, 6, 23);
+//    graph.setEdge(2, 7, 43);
+//    graph.setEdge(2, 8, 41);
+//    graph.setEdge(2, 9, 29);
+//    graph.setEdge(2, 10, 79);
+//    graph.setEdge(2, 11, 21);
+//    graph.setEdge(2, 12, 64);
+//
+//    graph.setEdge(3, 0, 46);
+//    graph.setEdge(3, 1, 46);
+//    graph.setEdge(3, 2, 68);
+//    graph.setEdge(3, 4, 82);
+//    graph.setEdge(3, 5, 15);
+//    graph.setEdge(3, 6, 72);
+//    graph.setEdge(3, 7, 31);
+//    graph.setEdge(3, 8, 62);
+//    graph.setEdge(3, 9, 42);
+//    graph.setEdge(3, 10, 21);
+//    graph.setEdge(3, 11, 51);
+//    graph.setEdge(3, 12, 51);
+//
+//    graph.setEdge(4, 0, 68);
+//    graph.setEdge(4, 1, 42);
+//    graph.setEdge(4, 2, 46);
+//    graph.setEdge(4, 3, 82);
+//    graph.setEdge(4, 5, 74);
+//    graph.setEdge(4, 6, 23);
+//    graph.setEdge(4, 7, 52);
+//    graph.setEdge(4, 8, 21);
+//    graph.setEdge(4, 9, 46);
+//    graph.setEdge(4, 10, 82);
+//    graph.setEdge(4, 11, 58);
+//    graph.setEdge(4, 12, 46);
+//
+//    graph.setEdge(5, 0, 52);
+//    graph.setEdge(5, 1, 43);
+//    graph.setEdge(5, 2, 55);
+//    graph.setEdge(5, 3, 15);
+//    graph.setEdge(5, 4, 74);
+//    graph.setEdge(5, 6, 61);
+//    graph.setEdge(5, 7, 23);
+//    graph.setEdge(5, 8, 55);
+//    graph.setEdge(5, 9, 31);
+//    graph.setEdge(5, 10, 33);
+//    graph.setEdge(5, 11, 37);
+//    graph.setEdge(5, 12, 51);
+//
+//    graph.setEdge(6, 0, 72);
+//    graph.setEdge(6, 1, 43);
+//    graph.setEdge(6, 2, 23);
+//    graph.setEdge(6, 3, 72);
+//    graph.setEdge(6, 4, 23);
+//    graph.setEdge(6, 5, 61);
+//    graph.setEdge(6, 7, 42);
+//    graph.setEdge(6, 8, 23);
+//    graph.setEdge(6, 9, 31);
+//    graph.setEdge(6, 10, 77);
+//    graph.setEdge(6, 11, 37);
+//    graph.setEdge(6, 12, 51);
+//
+//    graph.setEdge(7, 0, 42);
+//    graph.setEdge(7, 1, 23);
+//    graph.setEdge(7, 2, 43);
+//    graph.setEdge(7, 3, 31);
+//    graph.setEdge(7, 4, 52);
+//    graph.setEdge(7, 5, 23);
+//    graph.setEdge(7, 6, 42);
+//    graph.setEdge(7, 8, 33);
+//    graph.setEdge(7, 9, 15);
+//    graph.setEdge(7, 10, 37);
+//    graph.setEdge(7, 11, 33);
+//    graph.setEdge(7, 12, 33);
+//
+//    graph.setEdge(8, 0, 51);
+//    graph.setEdge(8, 1, 23);
+//    graph.setEdge(8, 2, 41);
+//    graph.setEdge(8, 3, 62);
+//    graph.setEdge(8, 4, 21);
+//    graph.setEdge(8, 5, 55);
+//    graph.setEdge(8, 6, 23);
+//    graph.setEdge(8, 7, 33);
+//    graph.setEdge(8, 9, 29);
+//    graph.setEdge(8, 10, 62);
+//    graph.setEdge(8, 11, 46);
+//    graph.setEdge(8, 12, 29);
+//
+//    graph.setEdge(9, 0, 55);
+//    graph.setEdge(9, 1, 31);
+//    graph.setEdge(9, 2, 29);
+//    graph.setEdge(9, 3, 42);
+//    graph.setEdge(9, 4, 46);
+//    graph.setEdge(9, 5, 31);
+//    graph.setEdge(9, 6, 31);
+//    graph.setEdge(9, 7, 15);
+//    graph.setEdge(9, 8, 29);
+//    graph.setEdge(9, 10, 51);
+//    graph.setEdge(9, 11, 21);
+//    graph.setEdge(9, 12, 41);
+//
+//    graph.setEdge(10, 0, 29);
+//    graph.setEdge(10, 1, 41);
+//    graph.setEdge(10, 2, 79);
+//    graph.setEdge(10, 3, 21);
+//    graph.setEdge(10, 4, 82);
+//    graph.setEdge(10, 5, 33);
+//    graph.setEdge(10, 6, 77);
+//    graph.setEdge(10, 7, 37);
+//    graph.setEdge(10, 8, 62);
+//    graph.setEdge(10, 9, 51);
+//    graph.setEdge(10, 11, 65);
+//    graph.setEdge(10, 12, 42);
+//
+//    graph.setEdge(11, 0, 74);
+//    graph.setEdge(11, 1, 51);
+//    graph.setEdge(11, 2, 21);
+//    graph.setEdge(11, 3, 51);
+//    graph.setEdge(11, 4, 58);
+//    graph.setEdge(11, 5, 37);
+//    graph.setEdge(11, 6, 37);
+//    graph.setEdge(11, 7, 33);
+//    graph.setEdge(11, 8, 46);
+//    graph.setEdge(11, 9, 21);
+//    graph.setEdge(11, 10, 65);
+//    graph.setEdge(11, 12, 61);
+//
+//    graph.setEdge(12, 0, 23);
+//    graph.setEdge(12, 1, 11);
+//    graph.setEdge(12, 2, 64);
+//    graph.setEdge(12, 3, 51);
+//    graph.setEdge(12, 4, 46);
+//    graph.setEdge(12, 5, 51);
+//    graph.setEdge(12, 6, 51);
+//    graph.setEdge(12, 7, 33);
+//    graph.setEdge(12, 8, 29);
+//    graph.setEdge(12, 9, 41);
+//    graph.setEdge(12, 10, 42);
+//    graph.setEdge(12, 11, 61);
+//
+//    BruteForce tsp;
+//    tsp.solve(graph);
+//
+//    ASSERT_EQ(1, tsp.getShortestPathLength());
+//    ASSERT_EQ("", tsp.toString());
+//}
